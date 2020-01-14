7c603bf357649338c910980de7eba2caf47a03cb
Kapil Rajak <kapil@Tesseract-2.local>
2020-01-15 00:10:00 +0530

    Spring boot app started

---
+++ src/main/java/io/kdkr/booklibrary/BookLibraryApiApp.java
@@ -0,0 +1,11 @@
+package io.kdkr.booklibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibraryApiApp {
+    public static void main(String[] args){
+        SpringApplication.run(BookLibraryApiApp.class, args);
+    }
+}
