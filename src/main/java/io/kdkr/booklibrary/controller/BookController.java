c353c2ac95f3ca170fb831214481d914e207a198
Kapil Rajak <kapil@Tesseract-2.local>
2020-01-15 16:06:49 +0530

    Redis introduced

--- src/main/java/io/kdkr/booklibrary/controller/BookController.java
+++ src/main/java/io/kdkr/booklibrary/controller/BookController.java
@@ -3,6 +3,9 @@ package io.kdkr.booklibrary.controller;
 import io.kdkr.booklibrary.model.*;
 import io.kdkr.booklibrary.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,20 +21,26 @@ public class BookController {
         return bookService.getAllBooks();
     }
 
+
+    //@Cacheable(value = "books", key = "#id", unless = "#result.followers < 12000")
+    @Cacheable(value = "books", key = "#id")
     @RequestMapping("/books/{id}")
     public Book getBook(@PathVariable String id)   {
         return bookService.getBook(id);
     }
+
     @RequestMapping(method = RequestMethod.POST, value= "/books")
     public void addBook(@RequestBody Book book)  {
         bookService.addBook(book);
     }
 
+    @CachePut(value = "books", key = "#book.id")
     @RequestMapping(method = RequestMethod.PUT, value= "/books/{id}")
     public void updateBook(@RequestBody Book book, @PathVariable String id)  {
         bookService.updateBook(id, book);
     }
 
+    @CacheEvict(value = "books", allEntries=true)
     @RequestMapping(method = RequestMethod.DELETE, value= "/books/{id}")
     public void updateBook(@PathVariable String id)  {
         bookService.deleteBook(id);
